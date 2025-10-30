a97cd6c7b5a26511d10d4d88a1298462358ab991
whitecalvin <whitecalvin@gxsoft.co.kr>
2025-10-30 13:56:33 +0900

    add server.py

---
+++ server.py
@@ -0,0 +1,14 @@
+# server.py
+from fastmcp import FastMCP
+
+mcp = FastMCP("Demo 🚀")
+
+
+@mcp.tool
+def add(a: int, b: int) -> int:
+    """Add two numbers"""
+    return a + b
+
+
+if __name__ == "__main__":
+    mcp.run(transport="http", host="0.0.0.0", port=8000)
